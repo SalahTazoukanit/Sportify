ec966e9b64c626d2c1834832d1e07deda0e4b311
SalahTazoukanit <salahtazu1996@gmail.com>
2024-09-07 11:32:44 +0200

    athentification added && EventController updated

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -32,6 +32,6 @@ class Event extends Model
     // Relation between users and events ;
     public function participants()
     {
-        return $this->belongsToMany(User::class, 'event_user', 'event_id', 'user_id');
+        return $this->belongsToMany(User::class);
     }
 }
