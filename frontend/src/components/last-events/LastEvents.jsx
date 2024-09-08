081f4415c386c206312b88d4077d4b17f4efe4fd
SalahTazoukanit <salahtazu1996@gmail.com>
2024-09-08 15:09:41 +0200

    event component added

--- frontend/src/components/last-events/LastEvents.jsx
+++ frontend/src/components/last-events/LastEvents.jsx
@@ -1,4 +1,10 @@
 const LastEvents = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h2>Nos derniers événements disponibles</h2>
+      </div>
+    </>
+  );
 };
 export default LastEvents;
