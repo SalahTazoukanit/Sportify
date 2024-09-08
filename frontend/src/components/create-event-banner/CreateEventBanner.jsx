f36f2f5ce05ed50f63a2fbd6bcce3e3165242775
SalahTazoukanit <salahtazu1996@gmail.com>
2024-09-08 17:39:37 +0200

    event banner added

---
+++ frontend/src/components/create-event-banner/CreateEventBanner.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+import "./CreateEventBanner.css";
+
+const CreateEventBanner = () => {
+  return (
+    <>
+      <div className="general-block flex w-full justify-center">
+        <div className="create-events hidden md:block md:w-1/2 md:h-96"></div>
+        <div className="flex flex-col sm:p-5 md:gap-10 m-5 w-full md:w-1/2 justify-center md:pl-12 mr-0 bg-third-color text-white">
+          <div className="flex flex-col md:gap-5">
+            <h2 className="font-bold">Créez votre propre Événement</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel,
+              fuga?
+            </p>
+          </div>
+          <NavLink>
+            <div className="bg-second-color w-52 h-10 flex justify-center rounded">
+              <button className="">Créez un événement</button>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+};
+export default CreateEventBanner;
--- frontend/src/views/homepage/Homepage.jsx
+++ frontend/src/views/homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import BannerTop from "../../components/top-banner/BannerTop";
 import DescriptionBanner from "../../components/description-banner/DescriptionsBanner";
 import Header from "../../components/header/Header";
 import LastEvents from "../../components/last-events/LastEvents";
+import CreateEventBanner from "../../components/create-event-banner/CreateEventBanner";
 
 const Homepage = () => {
   return (
@@ -11,6 +12,7 @@ const Homepage = () => {
         <BannerTop />
         <DescriptionBanner />
         <LastEvents />
+        <CreateEventBanner />
       </div>
       <div className="mb-40"></div>
     </>
