4393fd1e1caf4a030908c79e28a621c812fa28d9
SalahTazoukanit <salahtazu1996@gmail.com>
2024-09-12 21:02:45 +0200

    top-banner style updated

--- frontend/src/components/top-banner/BannerTop.jsx
+++ frontend/src/components/top-banner/BannerTop.jsx
@@ -1,13 +1,17 @@
+import "./BannerTop.css";
+
 const BannerTop = () => {
   return (
     <>
       <div className="general-block flex justify-center items-center">
         <div className="top-banner flex flex-col items-center w-11/12 rounded">
-          <div className="flex flex-col justify-center items-center text-second-color text-center mt-10">
-            <h1 className="tracking-wider	">
+          <div className=" flex flex-col justify-center items-center text-second-color text-center mt-10">
+            <h1 className="banner-top-text tracking-wider	">
               VOTRE RESEAU DE SPORTIFS <br />A PORTÉE DE MAIN
             </h1>
-            <h2 className="tracking-wider	">PENSEZ QUE A JOUER</h2>
+            <h2 className=" banner-top-text tracking-wider	">
+              PENSEZ QUE A JOUER
+            </h2>
           </div>
         </div>
       </div>
