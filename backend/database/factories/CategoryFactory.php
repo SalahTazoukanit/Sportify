ace74fbf990ce83a52bc5015a78877866b6c9fb9
SalahTazoukanit <salahtazu1996@gmail.com>
2024-09-22 13:19:06 +0200

    category factories changed

--- backend/database/factories/CategoryFactory.php
+++ backend/database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            "name" => fake()->name()
+            "name" => fake()->randomElement(['Football' ,'Ping-Pong','Basket','Musculation', 'Volleyball' , 'Vélo' , "Randonnée",'Balade'])
         ];
     }
 }
